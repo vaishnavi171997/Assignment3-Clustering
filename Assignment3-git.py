8aef04a088800530719bb62827f733636b1fc236
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2023-01-19 13:01:42 +0000

    Defining function to produce two data frames
    Defining function to produce two data frames for countries as column and years as column

--- Assignment3-git.py
+++ Assignment3-git.py
@@ -11,3 +11,20 @@ import pandas as pd
 import numpy as np
 #Importing matplotlib package for data plotting and visualization
 import matplotlib.pyplot as plt
+
+#defining function to produce two dataframes,one with countries as columns and one with years as coulmns
+def readcsv(input_file,countries):
+    data = pd.read_csv(input_file)
+    #replacing the null values with zeroes using fillna() method
+    dropping_values = data.fillna(0) 
+    test = dropping_values[dropping_values['Country Name'].isin(countries)]
+    df_countries = pd.DataFrame(test)
+    print(df_countries)
+    #transposing data to derive dataframe with years as columns
+    transpose=pd.DataFrame.transpose(test)
+    header = transpose.iloc[0].values.tolist()
+    transpose.columns = header
+    transpose = transpose.iloc[0:]
+    df_years = transpose
+    print(transpose)
+    return df_countries,df_years
